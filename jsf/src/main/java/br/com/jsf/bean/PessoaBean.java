59f9f62125fc4e04f0914d4afae6b506e1d310e4
wev <kleyton.cpd@gmail.com>
2019-07-06 18:05:25 -0300

    Modificações no PessoaBean e primeirapagina.xhtml

--- jsf/src/main/java/br/com/jsf/bean/PessoaBean.java
+++ jsf/src/main/java/br/com/jsf/bean/PessoaBean.java
@@ -1,18 +1,44 @@
 package br.com.jsf.bean;
 
-import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+@ViewScoped
 @ManagedBean(name = "pessoaBean")
 public class PessoaBean {
 
 	private String nome;
-	private String sobreNome;
-	private String nomeCompleto;
 	
+	private HtmlCommandButton commandButton;
+
+	private List<String> nomes = new ArrayList<String>();
+
+	public String addNome() {
+		nomes.add(nome);
+		if (nomes.size() > 2) {
+			commandButton.setDisabled(true);
+		}
+		return "";
+	}
 	
-	public String mostrarNome() {
-		nomeCompleto = nome + " " + sobreNome;
-		return nomeCompleto;
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+	
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
 	}
 
 	public String getNome() {
@@ -23,20 +49,5 @@ public class PessoaBean {
 		this.nome = nome;
 	}
 
-	public String getSobreNome() {
-		return sobreNome;
-	}
-
-	public void setSobreNome(String sobreNome) {
-		this.sobreNome = sobreNome;
-	}
-
-	public String getNomeCompleto() {
-		return nomeCompleto;
-	}
-
-	public void setNomeCompleto(String nomeCompleto) {
-		this.nomeCompleto = nomeCompleto;
-	}
 
 }
